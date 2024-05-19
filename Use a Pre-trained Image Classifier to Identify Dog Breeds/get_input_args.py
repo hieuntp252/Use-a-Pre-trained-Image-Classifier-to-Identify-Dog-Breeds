7b907036f3ad209da17219b3a9c10f003ad152b2
HieuNT <hieunguyentrung000@gmail.com>
2024-05-20 01:51:14 +0700

    done

--- Use a Pre-trained Image Classifier to Identify Dog Breeds/get_input_args.py
+++ Use a Pre-trained Image Classifier to Identify Dog Breeds/get_input_args.py
@@ -39,10 +39,13 @@ def get_input_args():
      parse_args() -data structure that stores the command line arguments object  
     """
     # Create Parse using ArgumentParser
-    
+    parser = argparse.ArgumentParser()
+
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
+    parser.add_argument("--div", type=str, default="pet_images/", help="path to the folder of pet images")
+    parser.add_argument("--arch", type=str, default="vgg", help="CNN model architecture")
+    parser.add_argument("--dogfile", type=str, default="dognames.txt", help="folder with a list of valid dognames")
     
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+    return parser.parse_args()
